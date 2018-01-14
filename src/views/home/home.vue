3e4a1ce207363e814551500b49953331bcf60cac
zzayne <thezhangwen@outlook.com>
2018-01-14 18:59:51 +0800

    init

---
+++ src/views/home/home.vue
@@ -0,0 +1,19 @@
+<style lang="less">
+    @import "./home.less";
+</style>
+<template>
+    <div class="home-main">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    data () {
+        return {
+            //
+        };
+    }
+};
+</script>
